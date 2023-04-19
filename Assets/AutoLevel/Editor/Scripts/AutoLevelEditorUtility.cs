6dcf801f4e5ab4fdb91e39d4b1d396b76651b520
ahmad al-asl <ahmad.alasl@yahoo.com>
2023-04-19 21:11:43 +0300

    some refactoring and adding menu buttons

---
+++ Assets/AutoLevel/Editor/Scripts/AutoLevelEditorUtility.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Formats.Fbx.Exporter;
+using UnityEngine;
+
+namespace AutoLevel
+{
+    public class AutoLevelEditorUtility
+    {
+        public static void ExportMesh(LevelBuilder builder, string path)
+        {
+            var so = new LevelBuilderEditor.SO(builder);
+            var repo = so.blockRepo.CreateRuntime();
+            var partationSize = LevelEditorSettings.GetSettings().settings.ExportSize;
+            ExportMesh(so, repo, path, partationSize);
+            so.Dispose();
+            repo.Dispose();
+        }
+
+        public static void ExportObjects(LevelBuilder builder, string path)
+        {
+            var so = new LevelBuilderEditor.SO(builder);
+            var repo = so.blockRepo.CreateRuntime();
+            ExportObjects(so, repo, path);
+            so.Dispose();
+            repo.Dispose();
+        }
+
+        public static void ExportMesh(LevelBuilderEditor.SO builder,BlocksRepo.Runtime repo,string path, int partationSize = 5)
+        {
+            using (LevelMeshBuilder mbuilder = new LevelMeshBuilder(builder.levelData, repo, partationSize))
+            {
+                mbuilder.Rebuild(new BoundsInt(Vector3Int.zero, builder.levelData.bounds.size));
+                ModelExporter.ExportObject(path, mbuilder.root.gameObject);
+            }
+        }
+
+        public static void ExportObjects(LevelBuilderEditor.SO builder, BlocksRepo.Runtime repo, string path)
+        {
+            using (var objectsBuilder = new LevelObjectsBuilder(builder.levelData, repo))
+            {
+                var bounds = builder.levelData.bounds;
+                bounds.position = Vector3Int.zero;
+                objectsBuilder.Rebuild(bounds);
+                PrefabUtility.SaveAsPrefabAsset(objectsBuilder.root.gameObject, path);
+            }
+        }
+    }
+}
--- Assets/AutoLevel/Runtime/Scripts/LevelObjectsBuilder.cs
+++ Assets/AutoLevel/Runtime/Scripts/LevelObjectsBuilder.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Security.Cryptography;
 using UnityEngine;
 
 namespace AutoLevel
